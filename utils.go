cb239467a509b10e442ecbaa7a287e3ac449864a
BALLUUNN <boldakovvova06@gmail.com>
2025-02-03 23:55:08 +0300

    fixed doc

--- utils.go
+++ utils.go
@@ -8,8 +8,8 @@ import (
 )
 
 const NumberOfSymbol int = 3
-//GetFloat reads a float-point number from the keyboard.
-//It returns the number read and any error encountered.
+// GetFloat reads a float-point number from the keyboard.
+// It returns the number read and any error encountered.
 func GetFloat() (float64, error) {
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
